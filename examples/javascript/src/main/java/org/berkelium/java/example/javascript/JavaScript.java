ffc7c08d82b593159e32e9a4fdf3cf1cc043f24d
Dennis Rieks <dennisrieks@googlemail.com>
2011-07-04 21:44:48 +0000

    javascript example

---
+++ examples/javascript/src/main/java/org/berkelium/java/example/javascript/JavaScript.java
@@ -0,0 +1,118 @@
+package org.berkelium.java.example.javascript;
+
+import org.berkelium.java.Berkelium;
+import org.berkelium.java.Window;
+import org.berkelium.java.WindowAdapter;
+
+/**
+ * Hello world!
+ */
+public class JavaScript {
+	public static void main(String[] args) throws InterruptedException {
+		System.err.println("creating runtime instance");
+		final Berkelium berkelium = Berkelium.createInstance();
+
+		System.err.println("creating window");
+		Window win = berkelium.createWindow();
+
+		System.err.println("setting window delegate");
+		win.setDelegate(new WindowAdapter() {
+			@Override
+			public void onLoad(Window win) {
+				System.err.println("onLoad");
+			}
+
+			@Override
+			public void onLoadingStateChanged(Window win, boolean isLoading) {
+				System.err.println("onLoadingStateChanged:" + isLoading);
+				if(!isLoading) {
+					//win.executeJavascript("window.externalHost.postMessage('test', 'urn:uuid:test123');");
+					win.executeJavascript("window.Berkelium('asyncCall','javafnk1', [])");
+				}
+			}
+
+			@Override
+			public void onStartLoading(Window win, String newURL) {
+				System.err.println("onStartLoading:" + newURL);
+			}
+
+			@Override
+			public void onAddressBarChanged(Window win, String newURL) {
+				System.err.println("onAddressBarChanged:" + newURL);
+			}
+
+			@Override
+			public void onTitleChanged(Window win, String newURL) {
+				System.err.println("onTitleChanged:" + newURL);
+			}
+			
+			@Override
+			public void onScriptAlert(Window win, String message,
+					String defaultValue, String url, int flags,
+					boolean[] success, String[] value) {
+				System.err.println("onScriptAlert:" + message);
+			}
+
+			@Override
+			public void onConsoleMessage(Window win, String message, String sourceId, int lineNo) {
+				System.err.printf("onConsoleMessage: '%s' (%s:%d)\n", message, sourceId, lineNo);
+			}
+
+			@Override
+			public void onJavascriptCallback(Window win, String url, String funcName) {
+				System.err.printf("onJavascriptCallback: '%s' (%s)\n", funcName, url);
+			}
+
+			@Override
+			public void onExternalHost(Window win, String message, String origin, String target) {
+				System.err.printf("onExternalHost: '%s'/'%s'/'%s'\n", message, origin, target);
+			}
+		});
+
+		update();
+
+		System.err.println("navigateTo");
+		win.navigateTo("http://www.google.de/search?q=hello%2C+world");
+		update();
+
+		update();
+		win.executeJavascript("console.log('hello, world!');");
+		update();
+		win.executeJavascript("test = function(){console.log('test!');}");
+		update();
+		win.executeJavascript("test();");
+		update();
+		win.executeJavascript("alert('alert test!');");
+
+		update();
+		win.bind("javafnk1", null);
+		update();
+		win.executeJavascript("console.dir(javafnk1);");
+		update();
+		win.addBindOnStartLoading("javafnk2", null);
+		update();
+		win.executeJavascript("javafnk2();");
+
+		update();
+		win.navigateTo("http://www.google.de/search?q=100+%E2%82%AC");
+		update();
+		win.executeJavascript("javafnk1();");
+		win.executeJavascript("javafnk2();");
+		update();
+
+		System.err.println("destroy");
+		berkelium.destroy();
+
+		System.err.println("done");
+	}
+
+	private static int count = 0;
+
+	private static void update() throws InterruptedException {
+		System.err.printf("\n\n===========================\n%d update loop\n", count++);
+		for (int i = 0; i < 50; ++i) {
+			Berkelium.getInstance().update();
+			Thread.sleep(10);
+		}
+	}
+}
